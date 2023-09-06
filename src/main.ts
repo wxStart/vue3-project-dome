e9919bc34bf49fd7e173944705e406e0d3bfbae8
wangxing <xing.wang@xforwardai.com>
2023-09-06 16:20:52 +0800

    feat: 引入element-plus

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
-//@ts-ignore
-// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
 
-// app.use(ElementPlus, {
-//   //@ts-ignore
-//   locale: zhCn
-// })
+app.use(ElementPlus, {
+  //@ts-ignore
+  locale: zhCn
+})
 
 app.use(createPinia())
 app.use(router)
