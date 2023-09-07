d636cf925e88e437bc1390a4317a0416cdd3adf9
wangxing <xing.wang@xforwardai.com>
2023-09-07 17:08:16 +0800

    fix: 接入mock插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { UserConfigExport, ConfigEnv } from 'vite'
+// import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import { viteMockServe } from 'vite-plugin-mock'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueJsx(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+export default ({ command }: ConfigEnv): UserConfigExport => {
+  console.log('command: ', command)
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      viteMockServe({
+        // default
+        mockPath: 'mock',
+        enable: command == 'serve'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
-})
+}
